ecdb5fad566e6d67b11ed1e9f3aef5b10ad5175c
MilesWes <wesleybantugan@gmail.com>
2024-09-11 15:10:34 -0700

    added a countdown

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date("09/21/2024 12:00").getTime()
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numMilliseconds = parseInt(launchTime - currentTime)
+  res.render('index', { title: 'A Banking App', countDown: numMilliseconds});
 });
 
 module.exports = router;
